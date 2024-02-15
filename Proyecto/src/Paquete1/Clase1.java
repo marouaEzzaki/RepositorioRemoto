d5a99f2ac164059b79d7d7fefb55ab38103fd95f
yassine <yassine@marwas-mbp.home>
2024-02-15 10:47:36 +0100

    Segundo commit Apartado A

--- Proyecto/src/Paquete1/Clase1.java
+++ Proyecto/src/Paquete1/Clase1.java
@@ -5,6 +5,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 
 		System.out.println("Primer print del apartado A del ejercicio 6");
+		System.out.println("Segundo print del apartado A (2do commit)");
 	}
 
 }
