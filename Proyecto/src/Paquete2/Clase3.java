b4628a54f2df1ec6d83ed3ae04e9eaa148ea61fc
yassine <yassine@localhost>
2024-02-17 23:57:29 +0100

    Commit en la nueva rama de 2 prints

---
+++ Proyecto/src/Paquete2/Clase3.java
@@ -0,0 +1,12 @@
+package Paquete2;
+
+public class Clase3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Print de la clase3");
+		System.out.println("Trabajando desde la Rama1");
+
+	}
+
+}
