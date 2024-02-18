0ade98481c356aa1e63456a5c7c1f1825c3b9eb4
yassine <yassine@localhost>
2024-02-18 09:01:57 +0100

    Commit 1 casa

---
+++ Proyecto/src/PaqueteCasa/Casa.java
@@ -0,0 +1,12 @@
+package PaqueteCasa;
+
+public class Casa {
+
+	public static void main(String[] args) {
+		
+		System.out.println("SOy un print de la clase CASA ");
+		System.out.println("Hacieno la práctica de entornos");
+
+	}
+
+}
